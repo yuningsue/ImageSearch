b25861a5d4967ae314f7f2aeabb546a5f8364173
Yu Ning Su <yuningsue@gmail.com>
2017-03-21 00:38:55 +0800

    fixed search bug

---
+++ index.js
@@ -0,0 +1,53 @@
+var vm = new Vue({
+  el: '#main-container',
+  data: {
+    images: [],
+    query: ''
+  },
+  methods: {    /* callFlickrPublic API */
+    callapi: function (query) {
+      var reqURL = "https://api.flickr.com/services/feeds/photos_public.gne";
+      var options = {
+        params: {
+          format: 'json',
+          tags: this.query
+        }
+      };
+
+      this.$http.jsonp(reqURL, options); /* sent http requet to flickr public api */  
+    }
+
+  }
+});
+
+/* the flickr default wrapped callback function */
+function jsonFlickrFeed(response) {
+  if(response.items.length > 0){
+    vm.$data.images = response.items;
+  }
+  else{
+    vm.$data.images = null;
+  }
+}
+
+
+/* add a custom directive called v-img */
+Vue.directive('img', {
+  inserted: function (el, binding) {
+    lazyload(el, binding);
+  },   /* inserted: called when bounded element has been inserted */
+  update: function (el, binding) {
+    lazyload(el, binding); /* update: called after images changed */
+  }
+});
+
+
+/* image preloading to improve user experience */
+function lazyload(el, binding) {
+  var img = new Image();   
+  img.src = binding.value;  /* update image source url */
+
+  img.onload = function() {
+    el.src = binding.value;   /* show photo after complete download */
+  };
+}
